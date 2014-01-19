7a8e9644df4f8e8149e480ea6392a6fb3edf93d2
Martin Vana <vanam@students.zcu.cz>
2014-01-19 15:59:29 +0100

    Converted tabs to spaces.

--- example.c
+++ example.c
@@ -9,7 +9,7 @@
 
 int comparator( const void *item_1, const void *item_2 )
 {
-	int value_1, value_2;
+        int value_1, value_2;
 
         value_1 = *( ( int * ) item_1 );
         value_2 = *( ( int * ) item_2 );
@@ -30,31 +30,31 @@ int comparator( const void *item_1, const void *item_2 )
 
 int main()
 {
-	priority_queue *pq = NULL;
-	int a = 55;
-	int b = 48;
-	int c = 1289;
-	int d = 78;
+        priority_queue *pq = NULL;
+        int a = 55;
+        int b = 48;
+        int c = 1289;
+        int d = 78;
 
-	int r = 0;
+        int r = 0;
 
-	/* 48 is minimal priority queue size */
-    	pq = create_priority_queue( 48, &comparator );
+        /* 48 is minimal priority queue size */
+        pq = create_priority_queue( 48, &comparator );
 
-    	priority_queue_insert( pq, &a );
-    	priority_queue_insert( pq, &b );
-    	priority_queue_insert( pq, &c );
-    	priority_queue_insert( pq, &d );
+        priority_queue_insert( pq, &a );
+        priority_queue_insert( pq, &b );
+        priority_queue_insert( pq, &c );
+        priority_queue_insert( pq, &d );
 
-    	r = *(int *) priority_queue_poll( pq );
+        r = *(int *) priority_queue_poll( pq );
 
-    	printf("First is %d\n", r);
+        printf("First is %d\n", r);
 
-	r = *(int *) priority_queue_poll( pq );
+        r = *(int *) priority_queue_poll( pq );
 
-    	printf("Second is %d\n", r);
+        printf("Second is %d\n", r);
 
-    	free_priority_queue( &pq );
+        free_priority_queue( &pq );
 
-    	return EXIT_SUCCESS;
+        return EXIT_SUCCESS;
 }
